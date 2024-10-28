ea6e04facf790b0a9ee5cda37eb82caeffb8bd2e
Marta Pérez Villamandos <martapervil@MacBook-Air-de-Marta-3.local>
2024-10-28 21:27:03 +0100

    peek modified - homework day 10

--- peek.sh
+++ peek.sh
@@ -1,3 +1,16 @@
-head -n $2 $1
-echo ...
-tail -n $2 $1
+if [[ -n $2 ]]
+then
+	numero=$2
+else
+	numero=3
+fi
+
+if [[ $(cat $1 | wc -l) -le $((2*numero)) ]]
+then 
+	cat $1
+else
+	echo Warning: file contains more lines than required
+	head -n $numero $1
+	echo ...
+	tail -n $numero $1
+fi
