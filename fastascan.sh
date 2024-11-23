c6758446016f0a32d0def82e728a231fdd6bb323
Marta Pérez Villamandos <martapervil@MacBook-Air-de-Marta-3.local>
2024-11-24 00:27:40 +0100

    first draft

---
+++ fastascan.sh
@@ -0,0 +1,34 @@
+#Establecemos por defecto que la carpeta sea la actual.
+if [[ -z $1 ]]; then fold=.; else fold=$1; fi
+
+#Establecemos por defecto que el número de líneas sea 0.
+if [[ -z $2 ]]; then N=0; else N=$2; fi
+
+
+echo There are $(find $fold -type f -name '*.fa' -or -name '*.fasta'| wc -l) fasta files in this folder. 
+
+find $fold -type f -name '*.fa' -or -name '*.fasta' | while read i;
+do
+	if [[ -s $i ]]; then
+		awk '/>/{print $1}' $i;
+		echo; fi; done
+
+
+#find $fold -type f -name '*.fa' -or -name '*.fasta' | while read i;
+#do 
+	#echo $i $'\t' $(if [[ -h $i ]]; then echo Symbolic link; else echo Real file; fi) $'\t' Number of sequences: $(awk '/>/{print "line"}' $i | wc -l); 
+#echo ; done
+
+
+
+
+#Para contar la longitud de las secuencias 
+#sed 's/[- \n]//g' $i | awk '/>/{print "-"} !/>/{print length($0)}' | while read j; do 
+	#if [[ $j == - ]]; 
+		#then N=0; else N=$((N+$j)); S=$N; fi; 
+	#if [[ $S -gt $N ]]; 
+		#then echo $S; fi; done
+
+
+#Por si se trata de la sequencia total
+#sed 's/[- \n]//g' example.fa | for i in $(awk '!/>/{print length($0)}'); do LEN=$((LEN + $i)); done; echo $LEN
