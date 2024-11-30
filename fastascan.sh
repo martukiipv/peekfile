ce1974efa4e57be2556413692a38ceacb3cf0a31
Marta Pérez Villamandos <martapervil@MacBook-Air-de-Marta-3.local>
2024-11-30 18:00:58 +0000

    main task finished

--- fastascan.sh
+++ fastascan.sh
@@ -1,36 +1,71 @@
 #Establecemos por defecto que la carpeta sea la actual.
-if [[ -z $1 ]]; then fold=.; else fold=$1; fi
+if [[ -z $1 ]]; then fold=. N=0;
+elif [[ -n $1 && -n $2 ]]; then fold=$1 N=$2;
+elif [[ -n $1 && -z $2 ]]; then
+	if [[ $1 != [0123456789] ]]; then fold=$1 N=0;
+	else
+		if ls $1 . > out254762; then fold=$1 N=0; #HAgo esta comprobación por si la carpeta es un número.
+		else fold=. N=$1;
+		fi;
+	fi;
+fi
 
-#Establecemos por defecto que el número de líneas sea 0.
-if [[ -z $2 ]]; then N=0; else N=$2; fi
+ls $fold . > out254762 || fold=. #por si la carpeta no existe
+rm out254762
 
-find $fold -type f -name '*.fa' -or -name '*.fasta' | while read i;
+echo carpeta: $fold
+echo numero:  $N
+echo
+echo
+echo ===================
+
+findfasta=$(find $fold -type f -name '*.fa' -or -name '*.fasta')
+
+for i in $findfasta;
 do
+	#Creamos el archivo all_id, donde guardamos todas las secuencias.
 	if [[ -s $i ]]; then
-		awk '/>/{print $1}' $i >> uniq_files
-		fi; done
+		awk '/>/{print $1}' $i >> all_id
+		fi; 
 
-echo In this folder there are:
-echo - $(find $fold -type f -name '*.fa' -or -name '*.fasta'| wc -l) fasta files.
-echo - $(cat uniq_files | sort | uniq | wc -l) uniques IDs.
-echo ============================
-rm uniq_files
-
-rm a
-find $fold -type f -name '*.fa' -or -name '*.fasta' | while read i;
-do 
+	#Y ahora vamos a crear un archivo con los headers para poderlo poner con columnas bonitas
 	seqs=$(awk '{ORS=""} !/>/{print $0}' $i | sed 's/[- ]//g');
-	echo $(basename $i) $(if [[ -h $i ]]; then echo Symbolic link; else echo Real file; fi) Number of sequences: $(awk '/>/{print "line"}' $i | wc -l) $(echo -n $seqs | wc -m) $(echo $seqs | if [[ -n $(grep -i [DEQHILKMFPSWV]) ]]; then 
+	(echo $(basename $i)^I$(if [[ -h $i ]]; then 
+		echo Symbolic link; 
+	else 
+		echo Real file; fi)^INumber of sequences: $(awk '/>/{print "line"}' $i | wc -l)^ITotal length: $(echo -n $seqs | wc -m)^I$(echo $seqs | if [[ -n $(grep -i [DEQHILKMFPSWV]) ]]; then 
 		echo Aminoacid sequence; 
 	else 
-		echo Genetic sequence; fi);
-	echo; 
+		echo Genetic sequence; fi)) >> a
 done
 
 
+echo In this folder there are:
+echo - $(echo $findfasta| wc -w) fasta files.
+echo - $(cat all_id | sort | uniq | wc -l) uniques IDs.
+echo 
+rm all_id
 
 
+for i in $findfasta;
+do
+	column -t -s'^I' a | awk -F’\t’ '$1~/example1.fasta/{print $0}'
 
+	if [[ $N -eq 0 ]]; then
+		continue
+	elif
+		[[ $(cat $i | wc -l) -le $((2*$N)) ]]; then
+		cat $i
+	else
+		echo Warning: file contains more lines than required | grep --color "Warning: file contains more lines than required"
+		head -n $N $i
+		echo ... | grep --color "..."
+		tail -n $N $i
+	fi;
+	echo; 
+done
+
+rm a
 
 
 
